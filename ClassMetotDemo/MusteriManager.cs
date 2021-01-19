e50a93405dd75ba7bfeeb6cf902f413eea2649a4
Emre Demir <emre2548@gmail.com>
2021-01-19 19:17:07 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteriBilgileri)
+        {
+            Console.WriteLine(musteriBilgileri.Adi + musteriBilgileri.Soyadi + "Musterisi Başarılı bir şekilde eklendi.");
+        }
+        public void MusteriGuncelle(Musteri musteriBilgileri)
+        {
+            Console.WriteLine(musteriBilgileri.Adi + "İsimli Müşterinin " + musteriBilgileri.TcNo + " TC nosuna ait hesabı güncellendi güncel bakiye " + musteriBilgileri.Bakiye);
+        }
+
+        public void MusteriSil(Musteri musteriBilgileri)
+        {
+            Console.WriteLine(musteriBilgileri.TcNo + " Numarasına sahip " + musteriBilgileri.Adi + musteriBilgileri.Soyadi + "Musterisi silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+
+            musteri.Id = 1;
+            musteri.Adi = "Hakan";
+            musteri.Soyadi = "Sonmez";
+            musteri.TcNo = "123456789012";
+            musteri.TelefonNumarasi = "5301114433";
+            musteri.Bakiye = 187;
+
+            Musteri musteriGuncelle = new Musteri();
+
+            musteriGuncelle.Id = 1;
+            musteriGuncelle.Bakiye = 300;
+
+            Musteri musteriSil = new Musteri();
+            musteriSil.Id = 1;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri);
+            musteriManager.MusteriGuncelle(musteriGuncelle);
+            musteriManager.MusteriSil(musteriSil);
+
+
+
+        }
+    }
+}
